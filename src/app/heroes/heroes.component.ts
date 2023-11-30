fbadb5f8d1e0528d4fa7031ef67db90128e979a3
Asad-K-NHS <asad.khan37@nhs.net>
2023-11-30 18:06:47 +0000

    Finished lesson 4 which now displays a message each time a hero is selected

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 import { HeroService } from '../hero.service';
 import { HEROES } from '../mock-heroes';
+import { MessageService } from '../message.service';
 
 @Component({
   standalone: true,
@@ -14,20 +15,24 @@ import { HEROES } from '../mock-heroes';
   imports: [NgFor, NgIf, UpperCasePipe, FormsModule, HeroDetailComponent],
 })
 export class HeroesComponent {
-  constructor(private heroService: HeroService) {}
-
-  heroes: Hero[] = [];
   selectedHero?: Hero;
+  heroes: Hero[] = [];
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-
-  getHeroes(): void {
-    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
-  }
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) {}
 
   ngOnInit(): void {
     this.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id = ${hero.id}`);
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
   }
 }
