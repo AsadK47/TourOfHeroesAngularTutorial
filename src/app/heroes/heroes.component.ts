9b86e8890c2bb50e542fb15f6b2111aea09165c4
Asad-K-NHS <asad.khan37@nhs.net>
2023-11-30 17:55:42 +0000

    Halfway through lesson 4 - changed to observable and asynchronous call

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,22 +1,33 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
 import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { HeroDetailComponent } from "../hero-detail/hero-detail.component";
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
+import { HeroService } from '../hero.service';
+import { HEROES } from '../mock-heroes';
 
 @Component({
-    standalone: true,
-    selector: 'app-heroes',
-    templateUrl: './heroes.component.html',
-    styleUrls: ['./heroes.component.css'],
-    imports: [NgFor, NgIf, UpperCasePipe, FormsModule, HeroDetailComponent]
+  standalone: true,
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css'],
+  imports: [NgFor, NgIf, UpperCasePipe, FormsModule, HeroDetailComponent],
 })
 export class HeroesComponent {
-  heroes = HEROES;
+  constructor(private heroService: HeroService) {}
+
+  heroes: Hero[] = [];
   selectedHero?: Hero;
-  
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
   }
 }
